ddcb16c5990a787874dda696e0aedb48e4deef98
fedfontana <federico.fontana2000@gmail.com>
2023-01-14 11:34:10 +0100

    --print option and fixes

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,14 @@ struct Args {
     #[arg(default_value_t=1, value_parser=clap::value_parser!(u64).range(1..))]
     number: u64,
 
+    /// If true, the program will print all of the results in order.
+    /// Works only if NUMBER > 1
+    #[arg(long, global(true))]
+    print: bool,
+
     /// the item you want to toss
     #[command(subcommand)]
-    tossable: TossOptions,
+    tossable: Option<TossOptions>,
 }
 
 #[derive(Subcommand)]
@@ -30,10 +35,16 @@ enum TossOptions {
     },
 }
 
+impl Default for TossOptions {
+    fn default() -> Self {
+        Self::Coin { heads_probability: 50 }
+    }
+}
+
 fn main() -> Result<(), &'static str> {
     let args = Args::parse();
 
-    match args.tossable {
+    match args.tossable.unwrap_or_default() {
         TossOptions::Coin { heads_probability } => {
             let coin = Coin::new(heads_probability);
             if args.number == 1 {
@@ -48,13 +59,16 @@ fn main() -> Result<(), &'static str> {
                     .count();
                 let tails_count = res.len() - heads_count;
 
-                println!(
-                    "Results: {}",
-                    res.iter()
-                        .map(|e| e.as_ref())
-                        .collect::<Vec<_>>()
-                        .join(", ")
-                );
+                if args.print {
+                    println!(
+                        "Results: {}",
+                        res.iter()
+                            .map(|e| e.as_ref())
+                            .collect::<Vec<_>>()
+                            .join(", ")
+                    );
+                }
+
                 println!("Got {heads_count} heads and {tails_count} tails");
             }
         }
@@ -67,11 +81,20 @@ fn main() -> Result<(), &'static str> {
                 let res = dice.toss_many(args.number);
 
                 let mut counts = vec![0; (dice.max + 1 - dice.min) as usize];
-                for r in res {
-                    counts[r as usize - 1] += 1;
+                for r in res.iter() {
+                    counts[*r as usize - 1] += 1;
                 }
 
-                println!("Results:");
+                if args.print {
+                    println!(
+                        "Results: {}",
+                        res.iter()
+                            .map(|r| r.to_string())
+                            .collect::<Vec<_>>()
+                            .join(", ")
+                    );
+                }
+
                 for (idx, count) in counts.iter().enumerate() {
                     println!(
                         "Got {} {count} time{}",
